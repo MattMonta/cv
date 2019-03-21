21435964fe3ec8a91c0a3dcf9f764347ac020e76
Matthieu Montagne <matthieumontagne@gmail.com>
2019-03-21 13:32:02 +0100

    pages Héloïse

---
+++ app/controllers/heloise_controller.rb
@@ -0,0 +1,2 @@
+class HeloiseController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/heloise'
   get 'pages/matt'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
